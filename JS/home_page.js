ef33c3a49dc6980c5ebdf6aebfd6c592ba8f419d
Asol05 <158200081+Asol05@users.noreply.github.com>
2025-10-09 00:46:33 +0700

    Update home_page.js

--- JS/home_page.js
+++ JS/home_page.js
@@ -4,23 +4,12 @@ var menu = null
 var sidebar = null
 var closesidebar = null
 var showsidebar = null
-var sidebarIsOpen = fales
-
-function hideshowsidebar(){
-    if(window.innerWidth > 560){
-        showsidebar.style.display = 'none'
-    }
-    else if(!sidebarIsOpen){
-        showsidebar.style.display = 'flex'
-    }
-}
 
 function pagelond(){
     menu = document.getElementById('menu')
     sidebar = document.getElementById('side-menubar')
     closesidebar = document.getElementById('closesidebar')
     showsidebar = document.getElementById('showsidebar')
-    setInterval(hideshowsidebar, 50);
     initbtn()
 }
 
@@ -28,18 +17,13 @@ function initbtn(){
     showsidebar.addEventListener("click" , () => {
         sidebar.style.display = 'flex'
         showsidebar.style.display = 'none'
-        sidebarIsOpen = true
     })
     closesidebar.addEventListener("click" , () =>{
         sidebar.style.display = 'none'
         showsidebar.style.display = 'flex'
-        sidebarIsOpen = fales
-
-        if(window.innerWidth > 560){
-            showsidebar.style.display = 'none'
-        }
     })
 }
+
 
 
 
