f3df5f30c249894702ff06f52807126f0da3532f
Asol05 <158200081+Asol05@users.noreply.github.com>
2025-10-09 00:07:07 +0700

    feat : upload JS and CSS

---
+++ JS/home_page.js
@@ -0,0 +1,30 @@
+console.log("init")
+window.onload = pagelond;
+
+var menu = null
+var sidebar = null
+var closesidebar = null
+var showsidebar = null
+
+function pagelond(){
+    menu = document.getElementById('menu')
+    sidebar = document.getElementById('side-menubar')
+    closesidebar = document.getElementById('closesidebar')
+    showsidebar = document.getElementById('showsidebar')
+    initbtn()
+}
+
+function initbtn(){
+    showsidebar.addEventListener("click" , () => {
+        sidebar.style.display = 'flex'
+        showsidebar.style.display = 'none'
+    })
+    closesidebar.addEventListener("click" , () =>{
+        sidebar.style.display = 'none'
+        showsidebar.style.display = 'flex'
+
+        if(window.innerWidth > 560){
+            showsidebar.style.display = 'none'
+        }
+    })
+}
